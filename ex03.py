95716d2a185d02d095028a0ad8f63968c5801eb8
Your Name <by.alejandro@gmail.com>
2023-10-06 13:17:04 +0200

    actualizo ex03, con números aleatorios sin repetición

--- ex03.py
+++ ex03.py
@@ -9,12 +9,12 @@ nombres = sample(range(-3, 11), 5)
 print("Llista: ")
 print(nombres[:])
 
-print("Quants números? "+str(len(nombres)))
+print("Quants nombres? "+str(len(nombres)))
 print("Quants 3? "+str(nombres.count(3)))
 print("Quants 3 i 4? \n3: "+str(nombres.count(3))+" vegades\n4: "+str(nombres.count(4))+" vegades")
 
 nombres.sort()
-print("Nombre mes gran? "+str(nombres[-1]))
+print("Nombre més gran? "+str(nombres[-1]))
 
 print("3 més petits? "+str(nombres[:3]))
 
